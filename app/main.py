974e0f352f346329d25679a01ac230020dc797ed
Amir Uleev <auleev@list.ru>
2025-05-31 09:18:49 +0300

    next_sta

--- app/main.py
+++ app/main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
 
 from ml.model import load_model
+from pydantic import BaseModel
 
 model = None
+
+class SentimentResponse(BaseModel):
+    text: str
+    sentiment_label: str
+    sentiment: float
 
 app = FastAPI()
 
@@ -10,7 +16,7 @@ app = FastAPI()
 def index():
     return {'text': 'somthing'}
 
-@app.lifespan('startup')
+@app.on_event('startup')
 def startup_event():
     global model
     model = load_model()
@@ -19,3 +25,9 @@ def startup_event():
 def predict_model(text: str):
     predict = model(text)
     
+    response = SentimentResponse(
+        text=text,
+        sentiment_label=predict.label,
+        sentiment_score=predict.score
+    )
+    return response
